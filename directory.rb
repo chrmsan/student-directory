c808dd1e2f7eea4daf838d5b2372c1c32a2e5364
Christopher Sanchez <sanchez.christopherm@gmail.com>
2016-09-13 14:26:10 +0200

    Added methods and called the methods

--- directory.rb
+++ directory.rb
@@ -13,10 +13,21 @@ students = [
 "Norman Bates"]
 # and then print them
 
+def print_header
   puts "The students of Villains Academy"
   puts "-------------"
-students.each {|student|
-  puts student
-}
-# finally, we print the total
-puts "Overall, we have #{students.count} great students"
+end
+
+def print(names) #adding the array variable "students" as an argument in this method
+  names.each {|student|
+    puts student
+  }
+end
+
+def print_footer(names)   #adding the students variable in here as argument
+  puts "Overall, we have #{names.count} great students"
+end
+# nothing happens until we call the methods, and we do it by writing the method names below
+print_header # first method in this file
+print(students) #second method in this file with the students varible as an argument
+print_footer(students) #third method with the students variable as an argument
