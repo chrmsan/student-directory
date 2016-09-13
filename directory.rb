67b501e66fce99b04659ec0aefa3bad8df69f38e
Christopher Sanchez <sanchez.christopherm@gmail.com>
2016-09-13 15:45:25 +0200

    Modified students array with hashes w/keys in each object and changed the print method to read keys

--- directory.rb
+++ directory.rb
@@ -1,15 +1,15 @@
-students = [
-["Dr. Hannibal Lecter", :november],
-["Darth Vader", :november],
-["Nurse Ratched", :november],
-["Michael Corleone", :november],
-["Alex DeLarge", :november],
-["The Wicked With of the West", :november],
-["Terminator", :november],
-["Freddy Krueger", :november],
-["The Joker", :november],
-["Joffrey Baratheon", :november],
-["Norman Bates", :november]
+students = [    #this is an array where each object in the array is a hash, with two keys and each key has a value
+{name: "Dr. Hannibal Lecter", cohort: :november},
+{name: "Darth Vader", cohort: :november},
+{name: "Nurse Ratched", cohort: :november},
+{name: "Michael Corleone", cohort: :november},
+{name: "Alex DeLarge", cohort: :november},
+{name: "The Wicked With of the West", cohort: :november},
+{name: "Terminator", cohort: :november},
+{name: "Freddy Krueger", cohort: :november},
+{name: "The Joker", cohort: :november},
+{name: "Joffrey Baratheon", cohort: :november},
+{name: "Norman Bates", cohort: :november}
 ]
 # and then print them
 
@@ -18,16 +18,16 @@ def print_header
   puts "-------------"
 end
 
-def print(names) #adding the array variable "students" as an argument in this method
-  names.each {|student|
-    puts student
-  }
+def print(students)
+  students.each {|student|        # here each object in the students array is called for, and the keys of the hash of each object is printed  in the print command puts  through interpolation 
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+        }
 end
 
-def print_footer(names)   #adding the students variable in here as argument
-  puts "Overall, we have #{names.count} great students"
+def print_footers(students)   #adding the students variable in here as argument
+  puts "Overall, we have #{students.count} great students"
 end
 # nothing happens until we call the methods, and we do it by writing the method names below
 print_header # first method in this file
 print(students) #second method in this file with the students varible as an argument
-print_footer(students) #third method with the students variable as an argument
+print_footers(students) #third method with the students variable as an argument
