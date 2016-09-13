b4f058279fb5b6e20443c9b1f4d7f6eef43587ee
Christopher Sanchez <sanchez.christopherm@gmail.com>
2016-09-13 15:03:30 +0200

    Updated students array

--- directory.rb
+++ directory.rb
@@ -1,16 +1,16 @@
-# let's put all students into an array
 students = [
-"Dr. Hannibal Lecter",
-"Darth Vader",
-"Nurse Ratched",
-"Michael Corleone",
-"Alex DeLarge",
-"The Wicked With of the West",
-"Terminator",
-"Freddy Krueger",
-"The Joker",
-"Joffrey Baratheon",
-"Norman Bates"]
+["Dr. Hannibal Lecter", :november],
+["Darth Vader", :november],
+["Nurse Ratched", :november],
+["Michael Corleone", :november],
+["Alex DeLarge", :november],
+["The Wicked With of the West", :november],
+["Terminator", :november],
+["Freddy Krueger", :november],
+["The Joker", :november],
+["Joffrey Baratheon", :november],
+["Norman Bates", :november]
+]
 # and then print them
 
 def print_header
