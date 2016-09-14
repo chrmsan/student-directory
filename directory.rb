f5be2dcc5a7a471eb40c5bd1773ca08038a735cd
Christopher Sanchez <sanchez.christopherm@gmail.com>
2016-09-14 15:54:52 +0200

    just for safekeeping - before doing part 4 in section 8

--- directory.rb
+++ directory.rb
@@ -1,37 +1,37 @@
 def print_header
-puts "The students of Villains Academy"
-puts "-------------"
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
 
 def input_students
-puts "Please enter the names of the students"
-puts "To finish, jusy hit return twice"
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
 # create an empty array
-students = Array.new
+  students = Array.new
 # get the first name
-name = gets.chomp
+  name = gets.chomp
 # while the name is not empty repeat ths code
-while !name.empty? 
+  while !name.empty? 
 #given that name variable is not empty, it is put as a key in the name: symbol inside a hash. And the hash itself is an object to the array students.
-students << {name: name, cohort: :november}
-puts "Now we have #{students.count} students"
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
 # and the code starts again where it asks for another name from the user
-name = gets.chomp
-end
+    name = gets.chomp
+  end
 # return the array of students
-students
+  students
 end
 
 def print(students) # default name of argument to the method print is only a name
-students.each_with_index {|student,i|
-if student[:name].length < 12
-  puts "#{i + 1}. #{student[:name]} (#{student[:cohort]} cohort)" #added +1 to index i so the index would start at 1."
-end 
-}
+students.each_with_index do |student,i|
+    if student[:name].length < 12
+      puts "#{i + 1}. #{student[:name]} (#{student[:cohort]} cohort)" #added +1 to index i so the index would start at 1."
+    end 
+  end
 end
 
 def print_footer(students) # counts the objects inside the students array
-puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{students.count} great students"
 end
 
 students = input_students # sets the input_student method as the variable students
