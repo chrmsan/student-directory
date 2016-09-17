be660d312d134ad4ab7a58a8e7a1dfc19c9a728e
Christopher Sanchez <sanchez.christopherm@gmail.com>
2016-09-17 12:21:54 +0200

    Final commit

--- directory.rb
+++ directory.rb
@@ -1,8 +1,3 @@
-def print_header
-  puts "The students of Villains Academy".center(50)
-  puts "".center(50, '-')
-end
-
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -41,38 +36,65 @@ def input_students
   students
 end
 
-def print(students) 
-    count = 1
 
-    while count <= students.length  
-      puts "#{count}. #{students[count-1][:name]} (#{students[count-1][:cohort]} cohort)" 
-      count += 1  
-    end 
+
+def print(students) 
+  if students.count > 0      
+
+      count = 1
+
+      while count <= students.length  
+        puts "#{count}. #{students[count-1][:name]} (#{students[count-1][:cohort]} cohort)" 
+        count += 1  
+      end
+  else
+    puts "Student list not available since no students were entered in the system."
+    puts "".center(50, '-')   
+  end 
 end
 
+
+
 def print_by_cohort(students)
-  puts "".center(50, '-')
-  puts "Here we will print out students from a specific cohort"
-  puts "Please enter cohort month of the relevant group of students"
-  cohort = gets.chomp.downcase.to_sym
-  puts "Students belonging to the #{cohort.capitalize} cohort of Villains Academy".center(50)
-  puts "".center(50, '-')
-  students.each do |student|
-    if student[:cohort] == cohort
-      puts "- #{student[:name]}, is #{student[:gender]}, who speaks #{student[:m_tongue]}, and is from #{student[:c_of_b]}"
+  if students.count > 0
+    puts "".center(50, '-')
+    puts "Here we will print out students from a specific cohort"
+    puts "Please enter cohort month of the relevant group of students"
+    cohort = gets.chomp.downcase.to_sym
+    puts "Students belonging to the #{cohort.capitalize} cohort of Villains Academy".center(50)
+    puts "".center(50, '-')
+    students.each do |student|
+      if student[:cohort] == cohort
+        puts "- #{student[:name]}, is #{student[:gender]}, who speaks #{student[:m_tongue]}, and is from #{student[:c_of_b]}"
+      end
     end
+  else
+    puts "Cohort student list not available since no students were entered in the system."
+    puts "".center(50, '-')
   end      
 end
 
 
+
+def print_header(students)
+  if students.count > 0  
+    puts "The students of Villains Academy".center(50)
+    puts "".center(50, '-')
+  end
+end
+
+
+
 def print_footer(students) 
-  puts "".center(50, '-')
-  puts "Overall, we have #{students.count} great #{students.count > 1 ? "students" : "student"}!".center(50)
-  puts "".center(50, '-')
+  if students.count > 0
+    puts "".center(50, '-')
+    puts "Overall, we have #{students.count} great #{students.count > 1 ? "students" : "student"}!".center(50)
+    puts "".center(50, '-')
+  end
 end
 
 students = input_students
-print_header
+print_header(students)
 print(students)
 #print_by_cohort(students) 
 print_footer(students) 
