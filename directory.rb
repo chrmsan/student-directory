049f9b2dfcfe7c625894327a9d395a432588be26
Christopher Sanchez <sanchez.christopherm@gmail.com>
2016-09-17 21:46:15 +0200

    Added load_students method

--- directory.rb
+++ directory.rb
@@ -152,6 +152,16 @@ end
 
 
 
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
+
 interactive_menu
 
 
