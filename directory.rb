eed0ec740e13d2e60e4056179b6a3009f750dcec
Christopher Sanchez <sanchez.christopherm@gmail.com>
2016-09-17 21:17:59 +0200

    Added save_students method

--- directory.rb
+++ directory.rb
@@ -133,6 +133,22 @@ def interactive_menu
   end
 end
 
+
+
+def save_students
+  # open the file for writing
+  file = File.open("student.csv", "w")     # open as in create a file?
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+
+
 interactive_menu
 #students = input_students   # creates a local variable called students that is equal to the method input_students, and there be equal to the students array of student hashes
 #print_header(students)      # method calls above students variable
